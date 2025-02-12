69e7e898562516a2c1cf2185cd1f104ba65ec643
) <pratyushparida36@gmail.com>
2025-02-12 13:11:32 +0530

    added page 4 and a loader animation

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 //     el: document.querySelector('#main'),
 //     smooth: true
 // });
+
+function loader() {
+    var loader = document.querySelector(".loader")
+    setTimeout(function () {
+        loader.style.top = "-100%"
+    }, 4200)
+}
+loader();`̀~~~~
